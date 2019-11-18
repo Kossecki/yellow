d95dd5d580087445c67f5c1989cbdbbcaca140ed
Kossecki <katarina.uzakova@protonmail.com>
2019-11-18 10:20:56 +0000

    function load project added

--- load-work.js
+++ load-work.js
@@ -1,22 +1,21 @@
 const button = document.getElementById("load-work");
-const originalNode = document.getElementById("article");
-const projectList = document.querySelector(".project-list")
-/* const newNode = originalNode.cloneNode(true); */
+const sourceElement = document.getElementById("main");	const originalNode = document.getElementById("article");
+const oldNode = document.getElementById("article");	const projectList = document.querySelector(".project-list");
+const newNode = document.importNode(oldNode, true);	
 
-const loadProject = function() {
-    let newNode = originalNode.cloneNode(true);
-    newNode.id="";
-    let newTitle = originalNode.querySelectorAll(."project-title").textContent
-    + " "
-    +projectList.querySelectorAll('article').length++;
+
+let load = function() {	const loadProject = function() {
+    document.getElementById("article").appendChild(newNode);	    let newNode = originalNode.cloneNode(true);
+    newNode.id = "";
+
+    let newTitle =
+        originalNode.querySelector(".project-title").textContent
+        + " "
+        + projectList.querySelectorAll('article').length++;
     newNode.querySelector(".project-title").innerHTML = newTitle;
-    console.log(newNode);
-    console.log(
-        document.querySelectorAll("article")
-    )
+
     projectList.insertBefore(newNode, null);
-};
+};	};
 
-button.addEventListener("click", loadProject);
 
-console.log(document.querySelector(".project-list-last article:last-of-type"));
+button.addEventListener("click", load);	button.addEventListener("click", loadProject);
