435fb48311bb2be9bf508e619389c1cd0108eb4e
Kossecki <katarina.uzakova@protonmail.com>
2019-11-18 12:23:18 +0000

    used prettier

--- load-work.js
+++ load-work.js
@@ -2,19 +2,17 @@ const button = document.getElementById("load-work");
 const originalNode = document.getElementById("article");
 const projectList = document.querySelector(".project-list");
 
-
 const loadProject = function() {
     let newNode = originalNode.cloneNode(true);
     newNode.id = "";
 
     let newTitle =
-        originalNode.querySelector(".project-title").textContent
-        + " "
-        + projectList.querySelectorAll('article').length++;
+        originalNode.querySelector(".project-title").textContent +
+        " " +
+        projectList.querySelectorAll("article").length++;
     newNode.querySelector(".project-title").innerHTML = newTitle;
 
     projectList.insertBefore(newNode, null);
 };
-
 
 button.addEventListener("click", loadProject);
